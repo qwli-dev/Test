0feed72109ae83d6bb52031b595e653d4044a793
qwli7 <228794754@qq.com>
2021-03-13 23:37:48 +0800

    httpRequest

--- src/main/java/com/qwli7/test/ui/HttpRequestTabbedPane.java
+++ src/main/java/com/qwli7/test/ui/HttpRequestTabbedPane.java
@@ -1,19 +1,24 @@
 package com.qwli7.test.ui;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class HttpRequestTabbedPane extends AbstractTabbedPane {
     @Override
     public JComponent createPane() {
 
-        JPanel jPanel = new JPanel(new GridLayout(1, 1));
+//        JPanel jPanel = new JPanel();
 
-        JLabel jLabel = new JLabel("HttpRequest");
-        jLabel.setFont(new Font(null, Font.PLAIN, 50));
-        jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        JSplitPane jSplitPane = new JSplitPane();
 
-        jPanel.add(jLabel);
-        return jPanel;
+        jSplitPane.setLeftComponent(new JButton("左边按钮"));
+        jSplitPane.setRightComponent(new JButton("右边按钮"));
+
+        jSplitPane.setOneTouchExpandable(true);
+
+        jSplitPane.setContinuousLayout(true);
+        jSplitPane.setDividerLocation(150);
+
+//        jPanel.add(jSplitPane);
+        return jSplitPane;
     }
 }
